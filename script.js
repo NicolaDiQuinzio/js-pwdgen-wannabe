7c61c989b779ed1034d678dfb8034c4b0361d4ba
NicolaDiQuinzio <nicola.di.quinzio1@gmail.com>
2023-03-07 14:59:42 +0100

    final commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = prompt("Come ti chiami?");
+let cognome = prompt("Qual è il tuo cognome?");
+let colore = prompt("Qual è il tuo colore preferito?");
+const numero = "21";
+
+let risultato = nome + cognome + colore + numero;
+
+document.write(risultato);
